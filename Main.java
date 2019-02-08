381c33a2a84a187f08a542d37e8b955231c8f29e
Alex Shirlaw <shirlawalex@gmail.com>
2019-02-08 11:38:39 +0100

    lecture des argumentss

--- Main.java
+++ Main.java
@@ -1,6 +1,42 @@
 public class Main{
 
 	public static void main(String[] args){
+		//Read of the args
+		int nb_args = args.length ;
+			
+		for(int i=0; i<nb_args;i++){
+			int len = agrs[i].length();
+			int start, end;
+
+			if(args[i].charAt(0) == ':'){
+				//Case 1 : All pax from 0 to n
+				start = 0;
+				//args[i].substring(1,len-1);
+				end = 
+			}else{
+				if(args[i].charAt(len-1) == ':'){
+					//Case 2 : All pax from n to last pax
+					System.out.println("Error : nombre limite de pax non connu");
+				}else{
+					boolean suite = false;
+					for(int j=0; j<args[i];i++){
+						if(args[i].charAt(j) == ':'){
+							suite = true;
+							start = ;
+							end = ;
+						}
+					}
+					if(suite){
+						//Case 3 : All pax from n to m
+
+					}else{
+						//Case 4 : Only pax n
+					}
+				}
+			}
+
+		}
+
 		String file = "Pax/pax"+args[0]+".txt";
 		Reader pax = new Reader(file);
 		Reader result = new Reader("result.txt");
