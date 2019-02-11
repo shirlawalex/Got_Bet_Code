1fcd6bf0b22b478ac912eaf67aa47c7629bb15ca
Alex Shirlaw <shirlawalex@gmail.com>
2019-02-11 15:45:49 +0100

    lecture des arguments opérationnels

--- Main.java
+++ Main.java
@@ -3,40 +3,78 @@ public class Main{
 	public static void main(String[] args){
 		//Read of the args
 		int nb_args = args.length ;
-			
-		for(int i=0; i<nb_args;i++){
-			int len = agrs[i].length();
-			int start, end;
+		System.out.println("nb args="+nb_args);
+		for(int i = 0;i< nb_args;i++){
+			System.out.println(args[i]);
+		}
 
+		for(int i=0; i<nb_args;i++){
+			int len = args[i].length();
+			int start =0;
+			int end = 0;			
+			
+			System.out.println("longueur de l'arg :"+len);
 			if(args[i].charAt(0) == ':'){
+				System.out.println("Case 1 : All pax from 0 to n");
 				//Case 1 : All pax from 0 to n
 				start = 0;
 				//args[i].substring(1,len-1);
-				end = Integer.parseInt(args[i].substring(1,len-1));
+				end = Integer.parseInt(args[i].substring(1,len));
+				System.out.println("debut: "+start+", fin: "+end);
 			}else{
 				if(args[i].charAt(len-1) == ':'){
+					System.out.println("Case 2 : All pax from n to last pax");
 					//Case 2 : All pax from n to last pax
+					end = -1;
+					start = Integer.parseInt(args[i].substring(0,len-1));
 					System.out.println("Error : nombre limite de pax non connu");
 				}else{
 					boolean suite = false;
-					for(int j=0; j<args[i];i++){
+					for(int j=0; j<len;j++){
 						if(args[i].charAt(j) == ':'){
 							suite = true;
-							start = Integer.parseInt(args[i].substring(0,j-1));
-							end = Integer.parseInt(args[i].substring(j+1,len));
+							String tmp1 = args[i].substring(0,j);
+							String tmp2 = args[i].substring(j+1,len);
+							System.out.println(tmp1 +" : "+ tmp2);							
+							start = Integer.parseInt(tmp1);
+							end = Integer.parseInt(tmp2);
+							
 						}
 					}
 					if(suite){
+						System.out.println("Case 3 : All pax from n to m");
 						//Case 3 : All pax from n to m
-
+						System.out.println("debut: "+start+", fin: "+end);
 					}else{
+						System.out.println("Case 4 : Only pax n");
 						//Case 4 : Only pax n
+						start =  Integer.parseInt(args[i]);
+						end = start;
+						System.out.println("debut: "+start+", fin: "+end);
 					}
 				}
 			}
 
-		}
+			for(int k=start;k<end+1;k++){
+				String file = "Pax/pax"+k+".txt";
+				Reader pax = new Reader(file);
+				Reader result = new Reader("result.txt");
 
+				Writer score = new Writer("Score/scorePax"+k+".txt");
+				String content = "";
+
+				content += pax.name + "\n";
+				System.out.println(pax.name);
+
+				int points = 0;
+				Checker.checkAll(pax,result,score);
+
+ 				score.close();
+			}
+		}
+		
+
+		/*
 		String file = "Pax/pax"+args[0]+".txt";
 		Reader pax = new Reader(file);
 		Reader result = new Reader("result.txt");
@@ -48,17 +86,19 @@ public class Main{
 		System.out.println(pax.name);
 
 		int points = 0;
-		/*
+		///////*
 		System.out.println("### Resultat Question ###");
 		points = Checker.checkQuestion(points,pax.question,result.question,score);
 		System.out.println("### Resultat Deathnothe ###");
 		points = Checker.checkDeathNote(points,pax.deathnote,result.deathnote,score);
  		System.out.println(points);
- 		*/
-
+ 		////////*/
+ 		//// 
+ 		/*
  		Checker.checkAll(pax,result,score);
 
  		score.close();
+ 		*/
  	} 
 	
 }
