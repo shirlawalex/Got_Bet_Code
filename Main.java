14c7b0bff2f33277c778ed6cdc24754495df6a10
Alex Shirlaw <shirlawalex@gmail.com>
2019-02-12 10:38:14 +0100

    suppression du code inutile v2

--- Main.java
+++ Main.java
@@ -55,50 +55,21 @@ public class Main{
 				}
 			}
 
+			//Lecture des pax(s) 
+
 			for(int k=start;k<end+1;k++){
 				String file = "Pax/pax"+k+".txt";
 				Reader pax = new Reader(file);
 				Reader result = new Reader("result.txt");
 
 				Writer score = new Writer("Score/scorePax"+k+".txt");
-				String content = "";
 
-				content += pax.name + "\n";
-				System.out.println(pax.name);
-
-				int points = 0;
+				//Fonction principale
 				Checker.checkAll(pax,result,score);
 
  				score.close();
 			}
 		}
-		
-
-		/*
-		String file = "Pax/pax"+args[0]+".txt";
-		Reader pax = new Reader(file);
-		Reader result = new Reader("result.txt");
-
-		Writer score = new Writer("Score/scorePax"+args[0]+".txt");
-		String content = "";
-
-		content += pax.name + "\n";
-		System.out.println(pax.name);
-
-		int points = 0;
-		///////*
-		System.out.println("### Resultat Question ###");
-		points = Checker.checkQuestion(points,pax.question,result.question,score);
-		System.out.println("### Resultat Deathnothe ###");
-		points = Checker.checkDeathNote(points,pax.deathnote,result.deathnote,score);
- 		System.out.println(points);
- 		////////*/
- 		//// 
- 		/*
- 		Checker.checkAll(pax,result,score);
-
- 		score.close();
- 		*/
  	} 
 	
 }
